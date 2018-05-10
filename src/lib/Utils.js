7d89bc6a6a40fb91db905dfc66fd9f13640ada1c
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2018-05-10 14:14:17 -0400

    Added more comments.

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -1,23 +1,37 @@
 import getDaysInMonth from 'date-fns/get_days_in_month';
 
+// Constructs a className from a set of prop keys.
 export const getClassName = (props) => {
   return Object.keys(props).filter((key) => props[key]).join(' ');
 };
-export const monthsOfYear = (locale='en-US') => {
-  window.months = window.months || [...Array(12)].map((_, i) => {
-    let baseDate = new Date(Date.UTC(2017, i + 1, 1));
 
+// Uses native JS dates to get the names of months of the year in a given locale.
+export const monthsOfYear = (locale='en-US') => {
+  // Cache result in window.months.
+  window.months = window.months || [...Array(12)].map((_, i) => {
+    // Get a date object set to the i+1th month.
+    let baseDate = new Date(Date.UTC(2017, i + 1, 1));
+    
+    // Get full name of this month.
     return baseDate.toLocaleDateString(locale, { month: 'long' });
   });
 
   return window.months;
 };
+
+// Returns number of days in a given month.
 export const daysInMonth = (month, year) => {
   return getDaysInMonth(new Date(year, month - 1));
 };
+
+// Returns an array of days of the week for a header (and Grid styles for IE compat).
 export const getMonthHeaderTemplate = (locale='en-US') => {
+  // Cache result in window.days.
   window.days = window.days || [...Array(7)].map((_, i) => {
-    const baseDate = new Date(Date.UTC(2017, 0, i + 2)); // just a Sunday
+    // Get a date object set to i+[random sunday offset]th day.
+    const baseDate = new Date(Date.UTC(2017, 0, i + 2));
+    
+    // Get full name of this day.
     const dayName = baseDate.toLocaleDateString(locale, { weekday: 'long' });
 
     return {
@@ -31,6 +45,8 @@ export const getMonthHeaderTemplate = (locale='en-US') => {
 
   return window.days;
 };
+
+// Returns an array of days of the month (and Grid styles for IE compat).
 export const getMonthTemplate = (month, year) => {
   // Number of days in month.
   const numDaysInMonth = daysInMonth(month, year);
